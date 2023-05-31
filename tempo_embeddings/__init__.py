d35293eab41d9569f4c35ab721391ba61c9618a9
Carsten Schnober <c.schnober@esciencecenter.nl>
2023-05-31 13:58:00 +0200

    Initial commit

---
+++ tempo_embeddings/__init__.py
@@ -0,0 +1,9 @@
+"""Documentation about tempo_embeddings"""
+import logging
+
+
+logging.getLogger(__name__).addHandler(logging.NullHandler())
+
+__author__ = "Carsten Schnober"
+__email__ = "c.schnober@esciencecenter.nl"
+__version__ = "0.0.1"
