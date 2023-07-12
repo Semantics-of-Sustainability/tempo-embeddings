1bb58c67a352cf58b1f3a322c499aed3753d85cb
Carsten Schnober <c.schnober@esciencecenter.nl>
2023-07-12 09:48:14 +0200

    Organize tests.

--- tempo_embeddings/__init__.py
+++
@@ -1,9 +0,0 @@
-"""Documentation about tempo_embeddings"""
-import logging
-
-
-logging.getLogger(__name__).addHandler(logging.NullHandler())
-
-__author__ = "Carsten Schnober"
-__email__ = "c.schnober@esciencecenter.nl"
-__version__ = "0.0.1"
