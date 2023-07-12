2c7e14828fd277ae116746d30f3acbe666a003e3
Carsten Schnober <c.schnober@esciencecenter.nl>
2023-07-12 13:39:42 +0200

    Split into unit and integration tests.

--- tests/integration/embeddings/test_model.py
+++ tests/integration/embeddings/test_model.py
@@ -1,14 +1,11 @@
 import numpy as np
 import pytest
 from tempo_embeddings.embeddings.model import RobertaModelWrapper
-from tempo_embeddings.text.corpus import Corpus
 from tempo_embeddings.text.corpus import TokenInfo
 from tempo_embeddings.text.passage import Passage
 
 
 class TestRobertaModelWrapper:
-    @pytest.mark.integration
-    @pytest.mark.slow
     @pytest.mark.parametrize(
         "model_name, text, char_span, expected",
         [
@@ -1570,22 +1567,3 @@ class TestRobertaModelWrapper:
         assert RobertaModelWrapper.from_pretrained(model_name).token_embedding(
             Passage(text), char_span
         ) == pytest.approx(expected, abs=1e-3)
-
-    @pytest.mark.parametrize(
-        "model_name, corpus",
-        [
-            ("roberta-base", Corpus()),
-            ("roberta-base", Corpus.from_lines(["This is a test."])),
-            ("roberta-base", Corpus({Passage("This is a test."): {TokenInfo(0, 4)}})),
-            (
-                "roberta-base",
-                Corpus(
-                    {Passage("This is a test."): {TokenInfo(0, 4), TokenInfo(5, 7)}}
-                ),
-            ),
-        ],
-    )
-    def test_add_embeddings(self, model_name, corpus):
-        RobertaModelWrapper.from_pretrained(model_name).add_embeddings(corpus)
-        for token_info in corpus.token_infos:
-            assert token_info.embedding is not None
