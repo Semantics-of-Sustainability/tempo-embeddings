46facfac98a39827f1d4ef4bb0c7d83cc0539f46
Angel Daza <j.daza@esciencecenter.nl>
2024-07-18 10:25:52 +0200

    separate chroma from weaviate loading

--- tempo_embeddings/embeddings/chroma_database.py
+++ tempo_embeddings/embeddings/chroma_database.py
@@ -16,7 +16,6 @@ from ..text.highlighting import Highlighting
 from ..text.passage import Passage
 from .vector_database import VectorDatabaseManagerWrapper
 
-
 logger = logging.getLogger(__name__)
 
 class ChromaDatabaseManager(VectorDatabaseManagerWrapper):
