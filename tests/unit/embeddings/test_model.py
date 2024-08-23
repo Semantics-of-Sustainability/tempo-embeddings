2ac508c7d617dc4d36d469691e11316280ca452d
Carsten Schnober <c.schnober@esciencecenter.nl>
2024-08-23 16:42:18 +0200

    Skip model test on MacOS for Github Runners.

--- tests/unit/embeddings/test_model.py
+++ tests/unit/embeddings/test_model.py
@@ -1,8 +1,16 @@
+import platform
+
+import pytest
+
 from tempo_embeddings.embeddings.model import SentenceTransformerModelWrapper
 from tempo_embeddings.settings import DEFAULT_LANGUAGE_MODEL
 
 
 class TestSentenceTransformerModelWrapper:
+    @pytest.mark.skipif(
+        platform.platform().startswith("macOS"),
+        reason="MacOS Github Runners don't have enough memory.",
+    )
     def test_embed_corpus(self, corpus):
         model = SentenceTransformerModelWrapper.from_pretrained(DEFAULT_LANGUAGE_MODEL)
         embedding_batches = list(model.embed_corpus(corpus))
