7f21a371fb0ede95ef2af59828c4f893d90bb84c
Carsten Schnober <c.schnober@esciencecenter.nl>
2024-08-27 16:52:25 +0200

    Test for missing year column warnings.

--- tests/unit/visualization/test_bokeh.py
+++ tests/unit/visualization/test_bokeh.py
@@ -1,13 +1,27 @@
+import logging
+
+import numpy as np
 from bokeh.models.widgets.sliders import RangeSlider
 
 from tempo_embeddings.visualization.bokeh import BokehInteractiveVisualizer
 
 
 class TestBokehInteractiveVisualizer:
-    def test_create_layout(self, corpus):
+    def test_create_layout(self, corpus, caplog):
+        corpus.embeddings_2d = np.random.rand(len(corpus), 2)
         visualizer = BokehInteractiveVisualizer(corpus)
-        layout = visualizer._create_layout()
-        assert layout.children == [visualizer._figure]
+
+        with caplog.at_level(logging.WARNING):
+            layout = visualizer._create_layout()
+            assert layout.children == [visualizer._figure]
+            assert caplog.record_tuples == [
+                (
+                    "root",
+                    logging.WARNING,
+                    "Column 'year' not found in cluster 'TestCorpus'.",
+                ),
+                ("root", logging.WARNING, "No year data found. Skipping year slider."),
+            ]
 
     def test_create_layout_years(self, corpus):
         year = 1999
